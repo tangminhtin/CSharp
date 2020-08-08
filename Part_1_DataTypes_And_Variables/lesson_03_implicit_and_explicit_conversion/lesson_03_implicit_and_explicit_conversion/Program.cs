b42772ba3ab664c0544fcb667b09ea2bb4623d47
tangminhtin <minhtintang@gmail.com>
2020-08-08 18:41:53 +0700

    Implicit and Explicit Conversion

---
+++ Part_1_DataTypes_And_Variables/lesson_03_implicit_and_explicit_conversion/lesson_03_implicit_and_explicit_conversion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace lesson_03_implicit_and_explicit_conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // implicit conversion
+            int num = 1253459324;
+            long bigNum = num;
+
+            float myFloat = 14.345f;
+            double myDouble = myFloat;
+
+
+            // explicit conversion
+            double myNewDouble = 3.14;
+            int myInt;
+            
+            myInt = (int)myNewDouble;   // cast double to int
+            Console.WriteLine(myInt);
+
+            // type conversion
+            string myString = myDouble.ToString();
+        }
+    }
+}

cf8804f401047439066a571b0653b75bb2d1e92b
tangminhtin <minhtintang@gmail.com>
2020-08-08 18:54:10 +0700

    Parsing a string to an integer

---
+++ Part_1_DataTypes_And_Variables/lesson_04_parsing_a_string_to_an_integer/lesson_04_parsing_a_string_to_an_integer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace lesson_04_parsing_a_string_to_an_integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myFirstString = "15";
+            string mySecondString = "14";
+            string result = myFirstString + mySecondString;
+
+            Console.WriteLine(result);
+
+            // parsing string to integer
+            int a = Int32.Parse(myFirstString);
+            int b = Int32.Parse(mySecondString);
+            int newResult = a + b;
+
+            Console.WriteLine(newResult);
+        }
+    }
+}

4f88a79cfc9599b6d750cb2c8c9f9e558258ee59
tangminhtin <minhtintang@gmail.com>
2020-08-08 19:52:17 +0700

    User input

---
+++ Part_2_Function_And_Method/lesson_03_user_input/lesson_03_user_input/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace lesson_03_user_input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a = ");
+            int a = int.Parse(Console.ReadLine());  // get input
+
+            Console.Write("Enter b = ");
+            int b = int.Parse(Console.ReadLine());  // get input
+
+            Console.WriteLine("Sum of {0} + {1} = {2}", a, b, Sum(a, b)) ;
+        }
+
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}

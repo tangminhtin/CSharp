6d6c2675ed42527c51ca0a1dd053fa5eeddba3ab
tangminhtin <minhtintang@gmail.com>
2020-08-08 19:40:12 +0700

    Method with return value

---
+++ Part_2_Function_And_Method/lesson_02_method_with_return_value/lesson_02_method_with_return_value/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace lesson_02_method_with_return_value
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sum: " + Add(5, 3));
+            Console.WriteLine("Multiple: " + Multiple(5, 3));
+            Console.WriteLine("Devide: " + Devide(5, 3));
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Multiple(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static double Devide(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}

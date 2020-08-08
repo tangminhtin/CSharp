d645ba4b2cb55725ca0009e26c18f34da3982346
tangminhtin <minhtintang@gmail.com>
2020-08-08 18:19:34 +0700

    String and some method

---
+++ Part_1_DataTypes_And_Variables/lesson_02_string_and_some_method/lesson_02_string_and_some_method/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace lesson_02_string_and_some_method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variable string
+            string myName = "Minh Tin Tang";
+            string message = "My name is " + myName;
+
+            Console.WriteLine(message);
+
+            // some method of string
+            Console.WriteLine(message.ToUpper());
+            Console.WriteLine(message.ToLower());
+            Console.WriteLine(message.Length);
+            Console.WriteLine(message.Contains("Tin"));
+        }
+    }
+}

33b1157b52d4d28a5e7dc1a4f6e9909781064f0f
tangminhtin <minhtintang@gmail.com>
2020-08-08 19:13:23 +0700

    Constants

---
+++ Part_1_DataTypes_And_Variables/lesson_06_constant/lesson_06_constant/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace lesson_06_constant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // constant is immutable values which are known
+            // at compile time and do not change for the life of the program.
+            const double PI = 3.14159265359;
+            const int weeks = 52, month = 12;
+            int days = 365;
+            const string birthday = "1999-29-09";
+
+            Console.WriteLine("My birthday is always going to be: {0}", birthday);
+        }
+    }
+}

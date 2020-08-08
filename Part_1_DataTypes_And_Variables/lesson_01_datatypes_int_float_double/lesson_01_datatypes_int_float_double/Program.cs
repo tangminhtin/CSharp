067a53bdc84ca782c94fe377b372ebb56b8999bd
tangminhtin <minhtintang@gmail.com>
2020-08-08 18:11:04 +0700

    DataTypes int, float and double

---
+++ Part_1_DataTypes_And_Variables/lesson_01_datatypes_int_float_double/lesson_01_datatypes_int_float_double/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace lesson_01_datatypes_int_float_double
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variable
+            int iA = 5, iB = 10;
+            float fA = 3.5f, fB = 4.3f;
+            double dA = 3.5, dB = 4.3;
+            
+            // set color for console
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Clear();
+
+            Console.WriteLine("The sum of [int]: " + iA + " + " + iB + " = " + (iA + iB));
+            Console.WriteLine("The sum of [float]: " + fA + " + " + fB + " = " + (fA + fB));
+            Console.WriteLine("The sum of [double]: " + dA + " + " + dB + " = " + (dA + dB));
+        }
+    }
+}

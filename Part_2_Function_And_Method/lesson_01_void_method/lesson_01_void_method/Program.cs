8146038acffc0be567857a832a33f7e497560932
tangminhtin <minhtintang@gmail.com>
2020-08-08 19:33:09 +0700

    Void method

---
+++ Part_2_Function_And_Method/lesson_01_void_method/lesson_01_void_method/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace lesson_01_void_method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sayHello();
+            welcome("Tang Minh Tin");
+        }
+
+        // access modifier (static) return type method name (parameter1, parameter2)
+        public static void sayHello()
+        {
+            Console.WriteLine("Hello Everyone!");
+        }
+
+        public static void welcome(string name)
+        {
+            Console.WriteLine("Welcome, {0} to my programing", name);
+        }
+    }
+}
